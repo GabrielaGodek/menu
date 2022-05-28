3d693987d314ee4175bcb4ac0b1c3a4255301a10
Gabriela Godek <83614025+GabrielaGodek@users.noreply.github.com>
2022-05-28 21:37:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuItems = document.querySelectorAll('.menu-box')
+
+menuItems.forEach(item => {
+    item.addEventListener('mouseover', () =>{
+        item.firstElementChild.style.backgroundColor = '#6f8186'
+        item.firstChild.nextElementSibling.nextElementSibling.nextElementSibling.style.color = '#3a4257'
+    })
+})
+menuItems.forEach(item => {
+    item.addEventListener('mouseout', () =>{
+        item.firstElementChild.style.backgroundColor = '#363d51'
+        item.firstChild.nextElementSibling.nextElementSibling.nextElementSibling.style.color = '#f0ddbc'
+})
+})
